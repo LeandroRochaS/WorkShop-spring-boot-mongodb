93df4304250524db670c0f33ce7c320e58a9c9ef
Leandro Rocha Santos <111878961+LeandroRochaS@users.noreply.github.com>
2023-05-12 11:10:23 -0300

    DTO Default User

--- src/main/java/com/projeto/workshop/demo/resources/UserResource.java
+++ src/main/java/com/projeto/workshop/demo/resources/UserResource.java
@@ -1,8 +1,10 @@
 package com.projeto.workshop.demo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 
+import com.projeto.workshop.demo.dto.UserDTO;
 import com.projeto.workshop.demo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,9 +22,10 @@ public class UserResource {
 	private UserService service;
 
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = (List<UserDTO>) list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
