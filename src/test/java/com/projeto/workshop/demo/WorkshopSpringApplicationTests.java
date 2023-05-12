033e49f7ea418f8ac09a75d2bd2cc02adee1dc10
Leandro Rocha Santos <111878961+LeandroRochaS@users.noreply.github.com>
2023-05-11 22:03:11 -0300

    First commit

---
+++ src/test/java/com/projeto/workshop/demo/WorkshopSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.workshop.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
